21ff256a770d091abe988910d2fa088dbe997150
Hermes <jack-ten@hotmail.com>
2021-08-09 13:55:02 -0300

    adding new rss feeds

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,37 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 /* Vanilla RSS - https://github.com/sdepold/vanilla-rss */
 
-	const rss = new RSS(
-	    document.querySelector("#rss-feeds"),
+	const rssMedium = new RSS(
+	    document.querySelector("#rss-feeds-Medium"),
+	    //Change this to your own rss feeds
+        "https://medium.com/feed/@jackson-ramalho",
+	    {
+		     // how many entries do you want?
+		    // default: 4
+		    // valid values: any integer
+		    limit: 3,
+		    
+		    
+		    // will request the API via https
+			// default: false
+			// valid values: false, true
+			ssl: true,
+		  
+			 // outer template for the html transformation
+			// default: "<ul>{entries}</ul>"
+			// valid values: any string
+			layoutTemplate: "<div class='items'>{entries}</div>",
+		
+			// inner template for each entry
+			// default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+			// valid values: any string
+			entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>',
+		    
+	    }
+	);
+	rssMedium.render();
+	const rssSO = new RSS(
+	    document.querySelector("#rss-feeds-SO"),
 	    //Change this to your own rss feeds
         "https://stackoverflow.com/feeds/user/10509394",
 	    {
@@ -37,7 +66,7 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 		    
 	    }
 	);
-	rss.render();
+	rssSO.render();
 
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
